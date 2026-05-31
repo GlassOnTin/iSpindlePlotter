e6c7f997a17d39716fe84b407b62ce1e681aed4e
Ian Williams <ianrosswilliams@gmail.com>
2026-05-31 20:29:09 +0100

    Release v0.5.15: calmer pause detection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ispindle.plotter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.5.14"
+        versionCode = 20
+        versionName = "0.5.15"
     }
 
     signingConfigs {
