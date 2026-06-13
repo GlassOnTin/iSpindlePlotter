f0d082bd852299358814ad5c76246874cc7cfe5e
Ian Williams <ianrosswilliams@gmail.com>
2026-06-14 00:16:26 +0100

    Release v0.5.18: buffering proxy with mDNS discovery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ispindle.plotter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.5.17"
+        versionCode = 23
+        versionName = "0.5.18"
     }
 
     signingConfigs {
